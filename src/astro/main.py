9b545e0ed3788d0922406d3d592d3c1826b34f70
Pavel Dusek <pavel.dusek@gmail.com>
2025-04-06 14:21:05 +0200

    new package

---
+++ src/astro/main.py
@@ -0,0 +1,32 @@
+"""A simple script to stay in touch with the space."""
+
+import datetime
+import subprocess
+from pathlib import Path
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main() -> None:
+    """Downloads the NASA picture of the day and shows it with gwenview."""
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    url = "https://apod.nasa.gov/"
+    path = Path("home/pavel/Pictures/nasa") / Path(f"{date}.jpg")
+
+    response = requests.get(url, timeout=15)
+    soup = BeautifulSoup(response.text, "html.parser")
+    img = soup.find("img")
+    src = img.attrs["src"]
+    alt = img.attrs["alt"]
+
+    print(alt)
+    response = requests.get(f"{url}{src}", stream=True, timeout=15)
+    with open(path, "wb") as f:
+        for chunk in response:
+            f.write(chunk)
+    subprocess.Popen(["gwenview", path])
+
+
+if __name__ == "__main__":
+    main()
